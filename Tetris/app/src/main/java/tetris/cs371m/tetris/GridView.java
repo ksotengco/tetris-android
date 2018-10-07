1b9b04db144c896ff43a31a3603fe8e6faf4c7d8
DESKTOP-1FEC649\Kyle-Laptop <ksotengco@gmail.com>
2018-10-07 16:21:52 -0500

    layout designed, drawing to game board works, does not account for invisible rows

---
+++ Tetris/app/src/main/java/tetris/cs371m/tetris/GridView.java
@@ -0,0 +1,132 @@
+package tetris.cs371m.tetris;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class GridView extends View {
+
+    private TGrid g;
+    private TGrid.CellVisitor cv;
+    private Canvas drawingCanvas;
+    private float x;
+    private float y;
+
+    private int counter;
+
+    Paint borderPaint;
+    Paint colorPaint;
+
+    private void init() {
+        x = 0.0f;
+        y = 0.0f;
+
+        counter = 0;
+
+        borderPaint = new Paint();
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setColor(Color.BLACK);
+
+        colorPaint = new Paint();
+        colorPaint.setStyle(Paint.Style.FILL);
+
+        cv = new TGrid.CellVisitor() {
+            @Override
+            public void visitCell(TCell tCell) {
+                if (tCell != null) {
+                    colorPaint.setColor(tCell.myColor);
+                    Log.d("Color", "Counter: " + ++counter);
+                    drawBlock(colorPaint);
+                } else {
+                    Log.d("Null", "Counter: " + ++counter);
+                    drawBlock(null);
+                }
+            }
+        };
+    }
+
+    public GridView(Context context) {
+        super(context);
+        init();
+    }
+
+    public GridView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GridView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setGrid(TGrid g) {
+        this.g = g;
+        invalidate();
+    }
+
+    /*public void setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
+        invalidate();
+    }*/
+
+    public void drawBlock (Paint paint) {
+        float dWidth = (float) this.getWidth()/(float) g.getWidth();
+        float dHeight = (float) this.getHeight()/(float) (g.getHeight() - 3);
+
+
+        // draws both border and square; border thicker if tetromino piece
+        if (paint != null) {
+            drawingCanvas.drawRect(x, y, x + dWidth, y + dHeight, paint);
+            borderPaint.setStrokeWidth(3.0f);  // arbitrary number; I just like this width for piece
+        } else {
+            borderPaint.setStrokeWidth(0.0f);
+        }
+
+        drawingCanvas.drawRect(x, y, x + dWidth, y + dHeight, borderPaint);
+
+        x += dWidth;
+
+        // - 1.0f used to account for rounding errors
+        float gameWidth = (float) this.getWidth() - 1.0f;
+        float gameHeight = (float) this.getHeight() - 1.0f;
+
+        if (x >= gameWidth) {
+            x = 0.0f;
+            y += dHeight;
+        }
+
+        if (y >= gameHeight) {
+            y = 0.0f;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawingCanvas = canvas;
+
+
+        if (g != null) {
+            int w = g.getWidth();
+            int h = g.getHeight();
+
+            Tetromino tm = TetrominoBuilder.I();
+            tm.insertIntoGrid(2, 5, g);
+
+            g.visitCells(cv);
+            //paint.setColor(g.getColor());
+            /*for (float i = 0; i < this.getWidth(); i+= draw_width) {
+                canvas.drawRect(i, y, i + draw_width, y + draw_height, paint);
+            }*/
+        }
+    }
+}
--- Tetris/app/src/main/java/tetris/cs371m/tetris/MainActivity.java
+++ Tetris/app/src/main/java/tetris/cs371m/tetris/MainActivity.java
@@ -2,12 +2,20 @@ package tetris.cs371m.tetris;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameView = (GridView) findViewById(R.id.game_board);
+        TGrid gameBoard = new TGrid(10, 23);
+
+        gameView.setGrid(gameBoard);
     }
 }
